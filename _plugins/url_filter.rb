b38b367c0fdda67f18374d0a158d1d6337ed7733
Timo Aaldriks <t.aaldriks@gmail.com>
2025-05-23 14:27:45 +0200

    added custom filter for project urls

---
+++ _plugins/url_filter.rb
@@ -0,0 +1,10 @@
+module Jekyll
+    safe true
+    module ProjectUrlFilter
+        def project_url(input)
+            input.gsub(/[^0-9A-Za-z]/, '-').gsub(/-{1,}/, '-').delete_suffix('-').delete_prefix('-').downcase
+        end
+    end
+end
+
+Liquid::Template.register_filter(Jekyll::ProjectUrlFilter)
